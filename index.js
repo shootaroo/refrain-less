50cdacac942393304217c45fc705d787ad3299fa
shootaroo <shotaro.tsubouchi@gmail.com>
2015-01-07 20:38:31 +0900

    Bypass less options

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var fast = require('fast.js');
 
 module.exports = function (text, content, callback) {
   var refrain = this;
-  less.render(text, fast.assign(content, {
+  less.render(text, fast.assign({
     paths: [refrain.options.srcDir],
     filename: content.filePath
-  }), function (err, output) {
+  }, refrain.options.less || {}, content), function (err, output) {
     if (err) err = util.format('Error: %s\n    at %s:%d:%d', err.message, err.filename, err.line, err.column);
     callback(err, output ? output.css : output);
   });
